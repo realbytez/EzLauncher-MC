34cf20134084171f4757ffb659945940fe9c3a5c
Bytez <167707744+realbytez@users.noreply.github.com>
2025-03-20 11:48:26 -0700

    Add files via upload

---
+++ EzLauncher (MC)/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace EzLauncher__MC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnlaunch = new Guna.UI2.WinForms.Guna2Button();
+            this.usernametextbox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.versionstextbox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2Panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnlaunch
+            // 
+            this.btnlaunch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnlaunch.BorderRadius = 7;
+            this.btnlaunch.BorderThickness = 1;
+            this.btnlaunch.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnlaunch.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnlaunch.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnlaunch.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnlaunch.FillColor = System.Drawing.Color.DarkGreen;
+            this.btnlaunch.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlaunch.ForeColor = System.Drawing.Color.Black;
+            this.btnlaunch.Location = new System.Drawing.Point(12, 311);
+            this.btnlaunch.Name = "btnlaunch";
+            this.btnlaunch.Size = new System.Drawing.Size(654, 43);
+            this.btnlaunch.TabIndex = 10;
+            this.btnlaunch.Text = "Launch";
+            this.btnlaunch.Click += new System.EventHandler(this.btnlaunch_Click_1);
+            // 
+            // usernametextbox
+            // 
+            this.usernametextbox.BorderColor = System.Drawing.Color.Black;
+            this.usernametextbox.BorderRadius = 7;
+            this.usernametextbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usernametextbox.DefaultText = "";
+            this.usernametextbox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.usernametextbox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.usernametextbox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernametextbox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernametextbox.FillColor = System.Drawing.Color.DarkGreen;
+            this.usernametextbox.FocusedState.BorderColor = System.Drawing.Color.Black;
+            this.usernametextbox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.usernametextbox.ForeColor = System.Drawing.Color.Black;
+            this.usernametextbox.HoverState.BorderColor = System.Drawing.Color.Black;
+            this.usernametextbox.Location = new System.Drawing.Point(12, 55);
+            this.usernametextbox.Name = "usernametextbox";
+            this.usernametextbox.PlaceholderForeColor = System.Drawing.Color.Black;
+            this.usernametextbox.PlaceholderText = "Username";
+            this.usernametextbox.SelectedText = "";
+            this.usernametextbox.Size = new System.Drawing.Size(654, 36);
+            this.usernametextbox.TabIndex = 11;
+            // 
+            // versionstextbox
+            // 
+            this.versionstextbox.BorderColor = System.Drawing.Color.Black;
+            this.versionstextbox.BorderRadius = 7;
+            this.versionstextbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.versionstextbox.DefaultText = "";
+            this.versionstextbox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.versionstextbox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.versionstextbox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.versionstextbox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.versionstextbox.FillColor = System.Drawing.Color.DarkGreen;
+            this.versionstextbox.FocusedState.BorderColor = System.Drawing.Color.Black;
+            this.versionstextbox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.versionstextbox.ForeColor = System.Drawing.Color.Black;
+            this.versionstextbox.HoverState.BorderColor = System.Drawing.Color.Black;
+            this.versionstextbox.Location = new System.Drawing.Point(12, 97);
+            this.versionstextbox.Name = "versionstextbox";
+            this.versionstextbox.PlaceholderForeColor = System.Drawing.Color.Black;
+            this.versionstextbox.PlaceholderText = "Version (example 1.21.4)";
+            this.versionstextbox.SelectedText = "";
+            this.versionstextbox.Size = new System.Drawing.Size(654, 36);
+            this.versionstextbox.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "EzLauncher (MC)";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2Panel1.BackColor = System.Drawing.Color.Black;
+            this.guna2Panel1.Location = new System.Drawing.Point(-5, 37);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(687, 1);
+            this.guna2Panel1.TabIndex = 14;
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 7;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.DragStartTransparencyValue = 1D;
+            this.guna2BorderlessForm1.HasFormShadow = false;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.DarkGreen;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(645, 9);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(21, 20);
+            this.guna2ControlBox1.TabIndex = 15;
+            this.guna2ControlBox1.Click += new System.EventHandler(this.guna2ControlBox1_Click);
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.DarkGreen;
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(618, 9);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(21, 20);
+            this.guna2ControlBox2.TabIndex = 16;
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel2.BorderColor = System.Drawing.Color.Black;
+            this.guna2Panel2.BorderRadius = 7;
+            this.guna2Panel2.BorderThickness = 2;
+            this.guna2Panel2.Controls.Add(this.label3);
+            this.guna2Panel2.Controls.Add(this.label2);
+            this.guna2Panel2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(84)))), ((int)(((byte)(0)))));
+            this.guna2Panel2.Location = new System.Drawing.Point(999, 999);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(466, 230);
+            this.guna2Panel2.TabIndex = 17;
+            this.guna2Panel2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(84)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(84)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label2.Location = new System.Drawing.Point(15, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(678, 366);
+            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.guna2ControlBox2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.versionstextbox);
+            this.Controls.Add(this.usernametextbox);
+            this.Controls.Add(this.btnlaunch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "EzLauncher (MC)";
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Button btnlaunch;
+        private Guna.UI2.WinForms.Guna2TextBox usernametextbox;
+        private Guna.UI2.WinForms.Guna2TextBox versionstextbox;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EzLauncher (MC)/Form1.cs
@@ -0,0 +1,73 @@
+﻿using CmlLib.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CmlLib.Core.Auth;
+using CmlLib.Core.ProcessBuilder;
+using System.Reflection.Emit;
+using System.Web.UI.WebControls;
+
+namespace EzLauncher__MC_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void guna2ControlBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void btnlaunch_Click_1(object sender, EventArgs e)
+        {
+            guna2Panel2.Visible = true;
+            guna2Panel2.Location = new Point(95, 74);
+
+            var path = new MinecraftPath("MC");
+            var launcher = new MinecraftLauncher(path);
+
+            // Update Label3 with the file progress
+            launcher.FileProgressChanged += (send, args) =>
+            {
+                string progressText = $"Name: {args.Name}\n" +
+                                      $"Type: {args.EventType}\n" +
+                                      $"Total: {args.TotalTasks}\n" +
+                                      $"Progressed: {args.ProgressedTasks}";
+                label2.Text = progressText; // Update Label3 with file progress
+            };
+
+            // Update Label3 with the byte progress
+            launcher.ByteProgressChanged += (send, args) =>
+            {
+                string byteProgressText = $"{args.ProgressedBytes} bytes / {args.TotalBytes} bytes";
+                label3.Text = byteProgressText; // Update Label3 with byte progress
+            };
+
+            await launcher.InstallAsync(versionstextbox.Text);
+
+            var process = await launcher.BuildProcessAsync(versionstextbox.Text, new MLaunchOption
+            {
+                Session = MSession.CreateOfflineSession(usernametextbox.Text),
+                MaximumRamMb = 4096
+            });
+
+            process.Start();
+            guna2Panel2.Visible = false;
+            guna2Panel2.Location = new Point(999, 999);
+        }
+
+    }
+    }
+
